13e02893051cf01c9fee12e8435c5302b85e361a
mshdabiola <mshdabiola@gmail.com>
2024-05-22 07:33:13 +0100

    finish

---
+++ template/js/app.js
@@ -0,0 +1,17 @@
+
+
+var isShow= false
+
+//
+// const text=document.getElementById("logo")
+// text.addEventListener("click",()=>{
+//   const nav= document.getElementById("nav")
+//
+//   const display= nav.style.display
+//
+//   if (display==="none"){
+//     nav.style.display="flex"
+//   }else{
+//     nav.style.display="none"
+//   }
+// })
